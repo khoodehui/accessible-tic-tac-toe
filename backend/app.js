20342d73e287a2659f1cf1b9edf9bf63a5edc1cb
dehui-surer <dehui.khoo@gmail.com>
2022-10-21 23:28:28 +0800

    Configure MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const router = require('./controller')
+const config = require('./config')
+
+console.log('Connecting to MongoDB')
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
 
 const app = express()
 app.use(cors())
